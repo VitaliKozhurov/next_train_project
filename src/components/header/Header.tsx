1179e4741d354c775dcfc79108801491160b7ea9
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-11 10:17:53 +0300

    feat: add layout and header

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,10 @@
+import s from './Header.module.scss'
+
+export const Header = () => {
+  return (
+    <header className={s.header}>
+      <h2>Inctagram or logo example</h2>
+      <h2>Select for language</h2>
+    </header>
+  )
+}
