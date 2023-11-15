ee8f66e460ee60aa0ea91366a0bb45370f24a719
VitaliKozhurov <vitalik24021992@gmail.com>
2023-11-15 15:26:12 +0300

    feat: add static image

--- src/entities/CharacterCard/ui/CharacterCard.tsx
+++ src/entities/CharacterCard/ui/CharacterCard.tsx
@@ -1,12 +1,23 @@
 import { CharacterType } from '@/shared'
 import Image from 'next/image'
+import aliveStatus from 'public/statuses/alive.svg'
+import deadStatus from 'public/statuses/dead.svg'
+import unknownStatus from 'public/statuses/unknown.svg'
 
 import s from './CharacterCard.module.scss'
 
 type Props = CharacterType
-export const CharacterCard = ({ image, name }: Props) => {
+
+const statusImages = {
+  Alive: aliveStatus,
+  Dead: deadStatus,
+  unknown: unknownStatus,
+}
+
+export const CharacterCard = ({ image, name, status }: Props) => {
   return (
     <div className={s.card}>
+      <Image alt={''} height={20} src={statusImages[status]} width={20} />
       <h2 className={s.cardTitle}>{name}</h2>
       <Image alt={`Character ${name}`} height={300} src={image} width={300} />
     </div>
